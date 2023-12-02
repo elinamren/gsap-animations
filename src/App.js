50e7779962833b4640b5909cad3e35b7471a8fa2
Elin Amrén <elin.amren@adeprimo.se>
2023-12-02 10:42:47 +0100

    Start files GSAP project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 function App() {
+  const container = useRef();
+  const tl = useRef();
+
+  const toggleTimeline = () => {
+    tl.current.reversed(!tl.current.reversed());
+  };
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray('.box');
+      tl.current = gsap
+        .timeline()
+        .to(boxes[0], { x: 120, rotation: 360 })
+        .to(boxes[1], { x: -120, rotation: -360 }, '<')
+        .to(boxes[2], { y: -166 })
+        .reverse();
+    },
+    [],
+    container
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+      <h1>animate anything</h1>
       </header>
+      <section className="boxes-container" ref={container}>
+        <h2>Use the button to toggle a Timeline</h2>
+        <div>
+          <button onClick={toggleTimeline}>Toggle Timeline</button>
+        </div>
+        <div className="box">Box 1</div>
+        <div className="box">Box 2</div>
+        <div className="box">Box 3</div>
+      </section>
     </div>
   );
 }
