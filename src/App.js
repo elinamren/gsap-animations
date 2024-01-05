8bf144069c60923aa69c8960a21942edaced66aa
Elin Amrén <elin.amren@adeprimo.se>
2024-01-05 18:41:02 +0100

    logo animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 function App() {
   const container = useRef();
@@ -25,20 +27,79 @@ function App() {
     container
   );
 
+  gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(".logo",
+      { 
+        y: "50vh",
+        scale: 6,
+      },
+      {
+        y: 0,
+        scale: 1,
+        scrollTrigger: {
+          trigger: ".content",
+          scrub: true,
+          start: "top bottom",
+          endTrigger: ".content",
+          end: "top center"
+        },
+      }
+    );
+  });
+
+  
+  // useEffect(() => {
+  //   const element = ref.current;
+  //   gsap.fromTo(element.querySelector(".logo"),
+  //     { 
+  //       y: "50vh",
+  //       scale: 6,
+  //     },
+  //     {
+  //       y: 0,
+  //       scale: 1,
+  //       scrollTrigger: {
+  //         trigger: element.querySelector(".content"),
+  //         scrub: true,
+  //         start: "top bottom",
+  //         endTrigger: element.querySelector(".content"),
+  //         end: "top center"
+  //       },
+  //     }
+  //   );
+  // }, []);
+
+
   return (
-    <div className="app">
-      <header className="app-header">
-      <h1>animate anything</h1>
-      </header>
-      <section className="boxes-container" ref={container}>
-        <h2>Use the button to toggle a Timeline</h2>
-        <div>
-          <button onClick={toggleTimeline}>Toggle Timeline</button>
+    <div className="app" ref={ref}>
+      <div className="logo-container">
+        <h1 className="logo">HELLO</h1>
+      </div>
+
+      <div className="container"></div>
+
+      <div className="content">
+        <img src="https://images.unsplash.com/flagged/photo-1563205764-79ea509b3e95?q=80&w=1861&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="rapper on stage"/>
+      </div>
+
+      <section className="section">
+        <div className="app-header">
+        <h2>animate anything</h2>
         </div>
-        <div className="box">Box 1</div>
-        <div className="box">Box 2</div>
-        <div className="box">Box 3</div>
+        <div className="boxes-container" ref={container}>
+          <h3>Use the button to toggle a Timeline</h3>
+          <div>
+            <button onClick={toggleTimeline}>Toggle Timeline</button>
+          </div>
+          <div className="box">Box 1</div>
+          <div className="box">Box 2</div>
+          <div className="box">Box 3</div>
+        </div>
       </section>
+      <section className="section-2"></section>
     </div>
   );
 }
